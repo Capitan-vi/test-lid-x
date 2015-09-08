8df0e1592166f48defffcfb7dff3f782be37c638
Valentin Emelyanov <valentin.html@gmail.com>
2015-09-08 16:11:35 +0300

    edited .portfolio-cases__short & .portfolio-cases__full to .portfolio-cases__projects

--- blocks/portfolio-cases/portfolio-cases.php
+++ blocks/portfolio-cases/portfolio-cases.php
@@ -5,17 +5,11 @@
 
       <!-- portfolio-cases__korpus-->
       <div class="portfolio-cases__korpus">
-        <input type="radio" name="portfolio-projects" id="tab1" checked>
-        <label class="portfolio-cases__korpus-tab" for="tab1">Проекты</label>
+        <input type="radio" name="portfolio-projects" id="portfolio-tab_projects" checked>
+        <label class="portfolio-cases__korpus-tab" for="portfolio-tab_projects">Проекты</label>
 
-        <input type="radio" name="portfolio-projects" id="tab2">
-        <label class="portfolio-cases__korpus-tab" for="tab2">Кратко</label>
-
-        <input type="radio" name="portfolio-projects" id="tab3">
-        <label class="portfolio-cases__korpus-tab" for="tab3">Подробно</label>
-
-        <input type="radio" name="portfolio-projects" id="tab4">
-        <label class="portfolio-cases__korpus-tab" for="tab4">Презентация</label>
+        <input type="radio" name="portfolio-projects" id="portfolio-tab_presentation">
+        <label class="portfolio-cases__korpus-tab" for="portfolio-tab_presentation">Презентация</label>
 
         <span class="portfolio-cases__helper">
           <span>Переключайтесь между режимами, чтобы увидеть больше подробностей</span>
@@ -24,11 +18,11 @@
 
 
         <!-- portfolio-cases__projects -->
-        <div class="portfolio-cases__projects">
+        <ul class="portfolio-cases__tab portfolio-cases__projects">
           <?php query_posts('post_type=case'); ?>
             <?php while ( have_posts() ) : the_post(); ?>
 
-            <div class="case__wrap">
+            <li class="case__wrap">
               <div class="case__head">
                 <div class="case__img-wrap"><img src="<?php the_field("case_logo"); ?>" alt=""></div>
                 <div class="case__info"><?php the_field("case_name"); ?></div>
@@ -81,125 +75,26 @@
                 </div>
               </div>
 
-            </div>
+            </li>
 
             <?php endwhile; ?>
           <?php wp_reset_query(); ?>
 
-        </div>
+        </ul>
+
+        <script>
+          $(document).ready(function() {
+            $('.case__wrap').click(function(e) {
+              e.preventDefault();
+              $(this).find('.case__additional-info').slideToggle();
+              $(this).toggleClass('case__wrap_full');
+            });
+          });
+        </script>
         <!-- END  portfolio-cases__projects -->
 
-        <!-- portfolio-cases__short-->
-        <div class="portfolio-cases__short">
-
-            <?php query_posts('post_type=case'); ?>
-              <?php while ( have_posts() ) : the_post(); ?>
-
-              <div class="case__wrap">
-                <div class="case__main">
-                  <div class="case__img-wrap"><img src="<?php the_field("case_logo"); ?>" alt=""></div>
-                  <div class="case__info mb-hidden"><?php the_field("case_name"); ?></div>
-                </div>
-
-                <div class="case__descr">
-                  <?php if(get_field('case_numbers')):
-                  while(has_sub_field('case_numbers')): ?>
-
-                    <div class="case__number-info">
-                    <div class="case__number <?php
-                        $arrow =  get_sub_field('arrow');
-                        if ($arrow == "arrow--up"):
-                        echo "case__number--up";
-                        elseif ($arrow == "arrow--down"):
-                        echo "case__number--down";
-                        endif
-                      ?>"><?php the_sub_field("case_number"); ?></div>
-                      <div >
-                        <?php the_sub_field("description"); ?>
-                      </div>
-                    </div>
-
-                  <?php endwhile; endif; ?> <!--end case_numbers repeater-->
-
-                </div>
-              </div>
-
-              <?php endwhile; ?>
-            <?php wp_reset_query(); ?>
-
-        </div>
-        <!-- END  portfolio-cases__short-->
-
-
-        <!-- portfolio-cases__full -->
-        <div class="portfolio-cases__full">
-          <?php query_posts('post_type=case'); ?>
-            <?php while ( have_posts() ) : the_post(); ?>
-
-            <div class="case__wrap">
-              <div class="case__head">
-                <div class="case__img-wrap"><img src="<?php the_field("case_logo"); ?>" alt=""></div>
-                <div class="case__info"><?php the_field("case_name"); ?></div>
-
-                <?php if(get_field('case_numbers')):
-                while(has_sub_field('case_numbers')): ?>
-
-                <div class="case__number-info">
-                  <div class="case__number"><?php the_sub_field('case_number'); ?></div>
-                  <p>
-                    <?php the_sub_field("description"); ?>
-                  </p>
-                </div>
-
-                <?php endwhile; endif; ?> <!--end case_numbers repeater-->
-
-              </div>
-              <div class="case__additional-info">
-                <div class="case__part--left">
-                  <div class="case__history">
-                    <h4>История работы</h4>
-                    <?php the_field("case_history"); ?>
-                  </div>
-                  <div class="case__used">
-                    <h4>Мы использовали</h4>
-
-                    <?php if(get_field('case_used')):
-                    while(has_sub_field('case_used')): ?>
-
-                    <div class="case__used-img">
-                      <img src="<?php the_sub_field('img'); ?>" alt="">
-                    </div>
-
-                    <?php endwhile; endif; ?> <!--end case_used repeater-->
-
-                  </div>
-                </div>
-                <div class="case__part--right">
-                  <div class="case__respond">
-                    <h4>Отзыв</h4>
-                    <blockquote>
-                      <?php the_field("case_review"); ?>
-                    </blockquote>
-                    <p class="case__respond-company"><?php the_field("review_person"); ?></p>
-                    <? if (get_field('recomendation')): ?>
-                    <a href="<?php the_field("recomendation"); ?>" class="btn btn-6"><img src="<?php the_field("recomendation"); ?>" class="case__respond-img" alt="">Показать письмо</a>
-                  <? else: ?><? endif; ?>
-
-                  </div>
-                </div>
-              </div>
-
-            </div>
-
-            <?php endwhile; ?>
-          <?php wp_reset_query(); ?>
-
-        </div>
-        <!-- END  portfolio-cases__full -->
-
-
         <!-- portfolio-cases__presentation -->
-        <div class="portfolio-cases__presentation">
+        <div class="portfolio-cases__tab portfolio-cases__presentation">
 
           <div class="portfolio-cases__presentation-wrap">
             <p>Скачайте нашу презентацию со всеми работами из портфолио,</br>чтобы изучить ее не торопясь и даже без доступа к Интернету</p>
